72a7a4c03eaeb8254ea0e3babc5c1c09cab38345
Jupiiter <jax.jupiiter@outlook.com>
2017-11-01 23:57:06 -0700

    git push

--- app.js
+++ app.js
@@ -3,22 +3,59 @@ $(document).ready(function(){
     var user = 'X';
     var bot = 'O';
     var player = bot;
+    var userArr = [];
+    var botArr = [];
+    var row=[], col=[], dia=[];
 
-    $("input[type=text").click(boxClicked);
+
+    row[0] = ["a", "b", "c"];
+    row[1] = ["d", "e", "f"];
+    row[2] = ["g", "h", "i"];
+    col[0] = ["a", "d", "g"];
+    col[1] = ["b", "e", "h"];
+    col[2] = ["c", "f", "i"];
+    dia[0] = ["a", "e", "f"];
+    dia[2] = ["c", "e", "g"];
+
+    var testArr = ["a", "f", "e", "d"];
+    var newArr = [];
+    if (testArr.toString() === (row[1].toString() || row[2].toString())) {
+        console.log("WINNING")
+    }
+
+
+        console.log(newArr);
+
+        if (newArr.length === 3) {
+            console.log("FOUND A WIN!");
+        }
+
+
+
+    $("input[type=text]").click(boxClicked);
 
     function boxClicked(){
+
         var id = $(`#${this.id}`);
-        currPlayer();
+        
+        if (player == user) {
+            botArr.push(this.id);
+            player = bot;
+        } else {
+            userArr.push(this.id);
+            player = user;
+        }
+
+        userArr.sort();
+        botArr.sort();
+
+        console.log(botArr);
+        console.log(userArr);
         var marked=id.val(player);
         id.attr("disabled", true);
 
+
+
     }
 
-    function currPlayer(){
-        if (player==user) {
-            player = bot;
-        } else {
-            player = user;
-        }
-    }
 });
