9eaccf9b5562f6bbf74d19b1b50a6e420f4dc8f7
Jupiiter <jax.jupiiter@outlook.com>
2017-11-05 18:44:42 -0800

    allow user to pick X or O

--- README.md
+++ README.md
@@ -2,9 +2,11 @@
 ### [Advanced Front End Development Project]
 -----
 ## To-do/Known bugs
-* Allow user to pick X or O
+* ~~Allow user to pick X or O~~
 * Clean up win();
 * Clean up code
 * Add animation for visual appeal
-* Change symbol of winning line
-* Prevent user from winningˇ
+* Change color of winning line
+* Prevent user from winning
+* Get rid of text input blinker when player clicks box
+* Activate bot
--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 $(document).ready(function(){
-    var user = 'X';
-    var bot = 'O';
-    var player = bot;
-    $("input[type=text]").click(boxClicked);
+    var user, bot;
+    var player;
+    var startBtn = $("#startBtn");
+    var board = $(".board");
+    var radioValue = $("input[name=playerValue]:checked");
+    var setUp = $(".setUp");
 
-    function boxClicked(){
+    var random=Math.floor(Math.random() * 9) + 1;
+    console.log(random);
+
+
+    startBtn.click(pickSign);
+
+    $("input[type=text]").click(play);
+
+    function play(){
         var id = $(`#${this.id}`);
         
         if (player == user) {
@@ -19,30 +29,88 @@ $(document).ready(function(){
         win();
     }
 
-});
+    function pickSign() {
+        user = $("input[name=playerValue]:checked").val();
 
-function win(player) {
-    // rows
-        if ($("#a").val() === "X" && $("#b").val() === "X" && $("#c").val() === "X") {
-            console.log("X WIN");
-            gameOver();
-        } else if ($("#a").val() === "O" && $("#b").val() === "O" && $("#c").val() === "O") {
-            console.log("O WIN");
-            gameOver();
-        } else if ($("#d").val() === "X" && $("#e").val() === "X" && $("#f").val() === "X") {
-            console.log("X WIN");
-            gameOver();
-        } else if ($("#d").val() === "O" && $("#e").val() === "O" && $("#f").val() === "O") {
-            console.log("O WIN");
-            gameOver();
-        } else if ($("#g").val() === "X" && $("#h").val() === "X" && $("#i").val() === "X") {
-            console.log("X WIN");
-            gameOver();
-        } else if ($("#g").val() === "O" && $("#h").val() === "O" && $("#i").val() === "O") {
-            console.log("O WIN");
-            gameOver();
+        console.log(user);
+        if (user == "X") {
+            bot = "O";
+            user="X";
+        } else {
+            bot = "X";
+            user="O";
         }
-    function gameOver() {
-        $("input[type='text']").attr("disabled", true);
+        setUp.fadeOut(100);
+        setTimeout(function(){
+            board.fadeIn(500);
+        },200);
+        
     }
-}
+
+    function win(player) {
+        // rows
+        
+            if ($("#1").val() === "X" && $("#2").val() === "X" && $("#3").val() === "X") {
+                console.log("X WIN");
+                gameOver();
+            } else if ($("#1").val() === "O" && $("#2").val() === "O" && $("#3").val() === "O") {
+                console.log("O WIN");
+                gameOver();
+            } else if ($("#4").val() === "X" && $("#5").val() === "X" && $("#6").val() === "X") {
+                console.log("X WIN");
+                gameOver();
+            } else if ($("#4").val() === "O" && $("#5").val() === "O" && $("#6").val() === "O") {
+                console.log("O WIN");
+                gameOver();
+            } else if ($("#7").val() === "X" && $("#8").val() === "X" && $("#9").val() === "X") {
+                console.log("X WIN");
+                gameOver();
+            } else if ($("#7").val() === "O" && $("#8").val() === "O" && $("#9").val() === "O") {
+                console.log("O WIN");
+                gameOver();
+
+            // column
+
+            } else if ($("#1").val() === "X" && $("#4").val() === "X" && $("#7").val() === "X") {
+                console.log("X WIN");
+                gameOver();
+            } else if ($("#1").val() === "O" && $("#4").val() === "O" && $("#7").val() === "O") {
+                console.log("O WIN");
+                gameOver();
+            } else if ($("#2").val() === "X" && $("#5").val() === "X" && $("#8").val() === "X") {
+                console.log("X WIN");
+                gameOver();
+            } else if ($("#2").val() === "O" && $("#5").val() === "O" && $("#8").val() === "O") {
+                console.log("O WIN");
+                gameOver();
+            } else if ($("#3").val() === "X" && $("#6").val() === "X" && $("#9").val() === "X") {
+                console.log("X WIN");
+                gameOver();
+            } else if ($("#3").val() === "O" && $("#6").val() === "O" && $("#9").val() === "O") {
+                console.log("O WIN");
+                gameOver();
+            
+
+            // diagonal
+
+            } else if ($("#1").val() === "X" && $("#5").val() === "X" && $("#9").val() === "X") {
+                console.log("X WIN");
+                gameOver();
+            } else if ($("#1").val() === "O" && $("#5").val() === "O" && $("#9").val() === "O") {
+                console.log("O WIN");
+                gameOver();
+            } else if ($("#3").val() === "X" && $("#5").val() === "X" && $("#7").val() === "X") {
+                console.log("X WIN");
+                gameOver();
+            } else if ($("#3").val() === "O" && $("#5").val() === "O" && $("#7").val() === "O") {
+                console.log("O WIN");
+                gameOver();
+            } 
+
+
+        function gameOver() {
+            $("input[type='text']").attr("disabled", true);
+        }
+    }
+
+});
